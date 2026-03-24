fdfd47ca4e1d502dc61fa9de7b02687e187b7343
taewok0205@gmail.com <taewok0205@gmail.com>
2026-03-24 22:16:02 +0900

    refactor: - div를 form 태그와 role="search"로 변경하여 검색 영역 정체성 정립 - 아이콘 영역을 label로 감싸 클릭 UX 및 웹 접근성 개선 - 인풋 폰트 크기 조정(text-sm) 및 요소별 고유 ID 부여

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -1,13 +1,33 @@
 import { Search } from "@/icons";
 
 export const SearchBar = () => (
-  <div className="relative flex items-center group">
+  <form
+    id="search-bar-form"
+    role="search"
+    className="relative flex items-center group"
+    onSubmit={(e) => e.preventDefault()} // 엔터 시 페이지 새로고침 방지
+  >
+    <label id="search-input-label" htmlFor="search-input">
+      검색어 입력
+    </label>
+
     <input
-      className="peer border cursor-pointer border-border-main w-[260px] h-10 px-4 pl-10 rounded-xl focus:outline-none focus:pl-4 transition-all placeholder:text-font2 focus:cursor-text"
+      id="search-input"
+      type="text"
+      className="text-sm border cursor-pointer border-border-main w-[260px] h-10 px-4 pl-10 rounded-xl focus:outline-none transition-all placeholder:text-font2 focus:cursor-text"
       placeholder="검색어를 입력하세요"
     />
-    <div className="absolute left-4 pointer-events-none peer-focus:hidden">
-      <Search className="text-font2" />
-    </div>
-  </div>
+
+    {/* 아이콘 영역을 label로 감싸 클릭 시 input에 포커스가 가도록 개선 */}
+    <label
+      id="search-icon-wrapper"
+      htmlFor="search-input"
+      className="absolute left-4 cursor-pointer pointer-events-none"
+    >
+      <Search
+        id="icon-search-glass"
+        className="text-font-disabled w-4.5 h-4.5 placeholder:text-font-disabled"
+      />
+    </label>
+  </form>
 );
