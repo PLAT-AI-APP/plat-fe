9eb9026b2dfea39abc54f9f02f25ff0f215c1e38
taewok0205@gmail.com <taewok0205@gmail.com>
2026-03-25 14:44:13 +0900

    feat: 소셜 로그인 공통 버튼 컴포넌트 구현

---
+++ src/components/auth/SocialLoginButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface SocialButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: React.ReactNode;
+  label: string;
+  onClick?: () => void;
+}
+
+const SocialLoginButton = ({ icon, label, onClick }: SocialButtonProps) => (
+  <button
+    id="social-login-button"
+    onClick={onClick}
+    className="relative w-full h-11.5 flex items-center justify-center rounded-lg border border-white/10 bg-black/20 shadow-[inset_0px_1px_100px_rgba(255,255,255,0.06)]"
+  >
+    <span
+      id="social-icon-wrapper"
+      className="absolute left-7.5 top-1/2 -translate-y-1/2"
+    >
+      {icon}
+    </span>
+    <span id="social-label" className="text-sm text-font-1 font-semibold">
+      {label}
+    </span>
+  </button>
+);
+
+export default SocialLoginButton;
