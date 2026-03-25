b5f6f70dddce0f4596309b645709f5b191db1c04
taewok0205@gmail.com <taewok0205@gmail.com>
2026-03-25 14:37:22 +0900

    feat: login form 외 모든 인증 form type 정의

---
+++ src/type/auth.ts
@@ -0,0 +1,24 @@
+export interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+export interface AuthFormValues extends Partial<LoginFormValues> {
+  // Step1 필드
+  otp: string[];
+
+  // Step2 필드
+  passwordConfirm?: string;
+
+  // Step3 필드
+  nickname: string;
+  gender: "MALE" | "FEMALE";
+  birthdate: string;
+}
+
+export interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+export type AuthMode = "SIGNUP" | "RESET_PASSWORD";
