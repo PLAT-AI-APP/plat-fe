37243528da7de6cef1f2a840b859d3cdaf38cb7e
taewok0205@gmail.com <taewok0205@gmail.com>
2026-03-25 14:44:48 +0900

    feat: 활성화/비활성화 제어 button 공통 컴포넌트

---
+++ src/components/ActiveButton.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import React, { ComponentPropsWithoutRef } from "react";
+
+// HTMLButtonElement의 모든 기본 속성을 포함하도록 확장
+interface ActiveButtonProps extends ComponentPropsWithoutRef<"button"> {
+  text: string;
+  isActive: boolean;
+  id: string;
+}
+
+const ActiveButton = ({
+  className,
+  text,
+  isActive,
+  type = "submit",
+  id,
+  ...props
+}: ActiveButtonProps) => {
+  return (
+    <button
+      id={id}
+      type={type}
+      disabled={!isActive}
+      className={cn(
+        "w-full h-11.5 font-semibold rounded-lg border border-white/10 transition-all",
+        isActive
+          ? "bg-brand text-white cursor-pointer"
+          : "bg-font-disabled text-font-1 cursor-not-allowed",
+        className,
+      )}
+      {...props}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default ActiveButton;
