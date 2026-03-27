b5dccbfe2d90860532a32c1113f004028e05173e
taewok0205@gmail.com <taewok0205@gmail.com>
2026-03-27 16:33:15 +0900

    fix: icon svg 피그마 변경 내용 반영

--- src/icons/ArrowDown.tsx
+++ src/icons/ArrowDown.tsx
@@ -4,13 +4,7 @@ import { IconProps, IconWrapper } from ".";
 const ArrowDown = (props: IconProps) => {
   return (
     <IconWrapper {...props}>
-      <path
-        d="M7 10L12 15L17 10"
-        stroke="inherit"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <path d="M17.4415 8.27037C17.798 7.90988 18.376 7.90988 18.7326 8.27037C19.0891 8.63086 19.0891 9.21519 18.7326 9.57568L12.6456 15.7296C12.289 16.0901 11.711 16.0901 11.3544 15.7296L5.26743 9.57568C4.91086 9.21519 4.91086 8.63086 5.26743 8.27037C5.624 7.90988 6.20197 7.90988 6.55854 8.27037L12 13.7717L17.4415 8.27037Z" />
     </IconWrapper>
   );
 };
--- src/icons/ArrowUp.tsx
+++ src/icons/ArrowUp.tsx
@@ -4,13 +4,7 @@ import { IconProps, IconWrapper } from ".";
 const ArrowUp = (props: IconProps) => {
   return (
     <IconWrapper {...props}>
-      <path
-        d="M17 14L12 9L7 14"
-        stroke="inherit"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <path d="M11.4234 8.20687C11.782 7.91111 12.3113 7.9326 12.6456 8.27057L18.7326 14.4244C19.0891 14.7848 19.0891 15.3692 18.7326 15.7296C18.376 16.0901 17.798 16.0901 17.4415 15.7296L12 10.2285L6.55854 15.7296C6.20197 16.0901 5.624 16.0901 5.26743 15.7296C4.91086 15.3692 4.91086 14.7848 5.26743 14.4244L11.3544 8.27057L11.4234 8.20687Z" />
     </IconWrapper>
   );
 };
