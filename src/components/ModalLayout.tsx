12e61d06ad2bd41de9b531216a4edec596ff7c67
taewok0205@gmail.com <taewok0205@gmail.com>
2026-04-01 18:30:55 +0900

    feat: triggerRef가 없을 경우 useClickAway hook의 close이벤트 제외

--- src/components/ModalLayout.tsx
+++ src/components/ModalLayout.tsx
@@ -1,15 +1,12 @@
-"use client";
-
 import { useClickAway } from "@/hooks/useClickAway";
+import { cn } from "@/lib/utils";
 import { useRef } from "react";
-
 interface ModalProps {
   children: React.ReactNode;
   onClose: () => void;
   className?: string;
   triggerRef?: React.RefObject<HTMLElement | null>; // 모달을 여는 버튼의 ref
 }
-
 export const ModalLayout = ({
   children,
   onClose,
@@ -18,15 +15,21 @@ export const ModalLayout = ({
 }: ModalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
-  // 생성한 ref를 훅에 전달
-  useClickAway(modalRef, onClose, triggerRef);
+  // triggerRef가 있을 때만 onClose를 실행하고, 없으면 아무것도 안 하는 빈 함수를 전달
+  // 이렇게 하면 훅 내부에서 이벤트가 발생해도 onClose가 호출되지 않습니다.
+  const handleClose = triggerRef ? onClose : () => {};
+
+  useClickAway(modalRef, handleClose, triggerRef);
 
   return (
     <section
       ref={modalRef}
       role="dialog"
       aria-modal="true"
-      className={`px-2 py-3 right-0 top-full translate-y-2.5 absolute z-10 bg-bg-dark rounded-xl shadow-card-heavy ${className || ""}`}
+      className={cn(
+        `px-2 py-3 right-0 top-full translate-y-2.5 absolute z-10 bg-bg-dark rounded-xl shadow-card-heavy`,
+        className,
+      )}
     >
       {children}
     </section>
