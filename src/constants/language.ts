4bfa1e72212c2d4b62abad0887d83f7046034d81
taewok0205@gmail.com <taewok0205@gmail.com>
2026-04-08 20:01:36 +0900

    feat: 언어 목록에 국가 코드 및 형식 추가

--- src/constants/language.ts
+++ src/constants/language.ts
@@ -1,8 +1,69 @@
-export const LANGUAGE_LIST = [
-  { code: "KR", name: "한국어", eng: "Korean" },
-  { code: "EN", name: "English", eng: "English" },
-  { code: "JP", name: "日本語", eng: "Japanese" },
-  { code: "CN", name: "中文", eng: "Chinese" },
-  { code: "TH", name: "ภาษาไทย", eng: "Thailand" },
-  { code: "VN", name: "Tiếng Việt", eng: "Vietnamese" },
+import { IconProps } from "@/icons";
+import { CNFlag, JPFlag, KRFlag, THFlag, USFlag, VNFlag } from "@/icons/flags";
+
+export interface LanguageItemType {
+  code: string;
+  name: string;
+  eng: string;
+  countryCode: string;
+  format: string;
+  length: number;
+  Icon: React.ComponentType<IconProps>;
+}
+
+export const LANGUAGE_LIST: LanguageItemType[] = [
+  {
+    code: "KR",
+    name: "한국어",
+    eng: "Korean",
+    countryCode: "+82",
+    format: "### #### ####",
+    length: 11,
+    Icon: KRFlag,
+  },
+  {
+    code: "EN",
+    name: "English",
+    eng: "English",
+    countryCode: "+1",
+    format: "###-###-####",
+    length: 10,
+    Icon: USFlag,
+  },
+  {
+    code: "JP",
+    name: "日本語",
+    eng: "Japanese",
+    countryCode: "+81",
+    format: "## #### ####",
+    length: 10,
+    Icon: JPFlag,
+  },
+  {
+    code: "CN",
+    name: "中文",
+    eng: "Chinese",
+    countryCode: "+86",
+    format: "### #### ####",
+    length: 11,
+    Icon: CNFlag,
+  },
+  {
+    code: "TH",
+    name: "ภาษาไทย",
+    eng: "Thailand",
+    countryCode: "+66",
+    format: "## #### ####",
+    length: 10,
+    Icon: THFlag,
+  },
+  {
+    code: "VN",
+    name: "Tiếng Việt",
+    eng: "Vietnamese",
+    countryCode: "+84",
+    format: "### #### ###",
+    length: 10,
+    Icon: VNFlag,
+  },
 ];
