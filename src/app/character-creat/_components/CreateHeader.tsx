9d5d2a2f5aacb928794f6612d7a4f34902ed9304
taewok0205@gmail.com <taewok0205@gmail.com>
2026-04-20 00:37:09 +0900

    feat: 뒤로가기 구현

--- src/app/character-creat/_components/CreateHeader.tsx
+++ src/app/character-creat/_components/CreateHeader.tsx
@@ -6,6 +6,7 @@ import ArrowLineLeft from "@/icons/ArrowLineLeft";
 import ActiveButton from "@/components/ActiveButton";
 import { useFormContext } from "react-hook-form";
 import { CharacterCreateFormValues } from "@/type/character";
+import { useRouter } from "next/navigation";
 
 interface CreateHeaderProps {
   onSave: () => void;
@@ -13,14 +14,33 @@ interface CreateHeaderProps {
 }
 
 const CreateHeader = ({ onSave, onDraftClick }: CreateHeaderProps) => {
+  const router = useRouter();
+
   const {
     formState: { isValid },
   } = useFormContext<CharacterCreateFormValues>();
 
+  const handleSafeBack = (fallbackPath: string = "/") => {
+    // 브라우저 세션 히스토리에 쌓인 페이지가 1개보다 많고,
+    // 이전 주소가 우리 서비스 도메인일 때만 뒤로 가기 수행
+    const isInternalNavigation = document.referrer.includes(
+      window.location.host,
+    );
+    const hasHistory = window.history.length > 1;
+
+    if (isInternalNavigation && hasHistory) {
+      router.back();
+    } else {
+      router.push(fallbackPath);
+    }
+  };
   return (
     <header className="flex items-center justify-between pb-4">
       <h2 className="flex items-center gap-2 text-[20px] font-medium">
-        <ArrowLineLeft className="w-6 h-6 text-font-2" />
+        <ArrowLineLeft
+          onClick={() => handleSafeBack("/")}
+          className="w-6 h-6 text-font-2 cursor-pointer"
+        />
         캐릭터 생성
       </h2>
 
