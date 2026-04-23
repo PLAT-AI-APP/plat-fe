ae8e90c97c3a585a9fe1988689145fa66d9a6fc4
taewok0205@gmail.com <taewok0205@gmail.com>
2026-04-23 19:44:23 +0900

    feat: 로그인 시 token을 useAuthStore에 저장

--- src/api/auth/emailLogin.ts
+++ src/api/auth/emailLogin.ts
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { axiosInstance } from "..";
 import { ApiSuccessResponse, AppError } from "@/type/api";
+import { useAuthStore } from "@/store/useAuthStore";
 
 interface PostEmailLoginProps {
   email: string;
@@ -8,19 +9,27 @@ interface PostEmailLoginProps {
 }
 
 const PostEmailLogin = async (props: PostEmailLoginProps) => {
-  const response = await axiosInstance.post<ApiSuccessResponse>(
-    "/auth/email/login",
-    props,
-  );
+  const response = await axiosInstance.post<
+    ApiSuccessResponse<{ accessToken: string }>
+  >("/auth/email/login", props);
 
   return {
     serverMessage: response.data.message ?? "",
+    token: response.data.data.accessToken,
   };
 };
 
 /** 이메일 로그인 */
 export const useEmailLoginMutation = () => {
-  return useMutation<{ serverMessage: string }, AppError, PostEmailLoginProps>({
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  return useMutation<
+    { serverMessage: string; token: string },
+    AppError,
+    PostEmailLoginProps
+  >({
     mutationFn: PostEmailLogin,
+    onSuccess: (data) => {
+      setAccessToken(data.token);
+    },
   });
 };
