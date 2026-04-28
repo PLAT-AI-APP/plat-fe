e634aa5d493cd1f719387d684da9b527556da1de
taewok0205@gmail.com <taewok0205@gmail.com>
2026-04-28 14:28:48 +0900

    feat: formatWithCommas적용

--- src/app/studio/[id]/_components/StudioStats.tsx
+++ src/app/studio/[id]/_components/StudioStats.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import { ArrowRight } from "@/icons";
+import { formatWithCommas } from "@/lib/utils";
 
 export const MOCK_STUDIO_DATA = {
-  characterCount: 1999999999,
-  chatCount: 1999999999,
+  characterCount: 329,
+  chatCount: 1455,
   isIdentityVerified: true, // 본인인증 완료
   isAdultVerified: false, // 미인증
 };
@@ -15,13 +16,17 @@ const StudioStats = () => {
       {/* 캐릭터 */}
       <div className="flex flex-1 flex-col gap-2 text-sm">
         <span className="text-font-2">캐릭터</span>
-        <span className="font-light">{MOCK_STUDIO_DATA.characterCount}</span>
+        <span className="font-light">
+          {formatWithCommas(MOCK_STUDIO_DATA.characterCount)}
+        </span>
       </div>
 
       {/* 채팅수 */}
       <div className="flex flex-1 flex-col gap-2 text-sm">
         <span className="text-font-2">채팅수</span>
-        <span className="font-light">{MOCK_STUDIO_DATA.chatCount}</span>
+        <span className="font-light">
+          {formatWithCommas(MOCK_STUDIO_DATA.chatCount)}
+        </span>
       </div>
 
       {/* 본인인증 */}
@@ -42,7 +47,7 @@ const StudioStats = () => {
             {MOCK_STUDIO_DATA.isAdultVerified ? "인증완료" : "미인증"}
           </span>
         </div>
-        {/* 화살표 아이콘 (오른쪽 끝) */}
+        {/* 화살표 아이콘 (오른쪽 끝) 추후 성인인증 로직이 생긴다면 경로 입력*/}
         <Link href={""} className="p-1 rounded-lg hover:bg-btn-hover">
           <ArrowRight className="w-3 h-3 text-font-2" />
         </Link>
