f507fa0f8a21df475dfb5cd23762f288e8fabe3f
taewok0205@gmail.com <taewok0205@gmail.com>
2026-04-29 15:41:43 +0900

    feat: ArrowLineRight icon 추가

---
+++ src/icons/ArrowLineRight.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { IconProps, IconWrapper } from ".";
+
+const ArrowLineRight = (props: IconProps) => {
+  return (
+    <IconWrapper {...props} viewBox="0 0 14 14">
+      <path
+        d="M7.58366 2.91683L11.667 7.00016M11.667 7.00016L7.58366 11.0835M11.667 7.00016L2.33366 7.00016"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </IconWrapper>
+  );
+};
+
+export default ArrowLineRight;
