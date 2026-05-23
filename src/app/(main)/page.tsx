ef4e58cf63a6a369c53b532e001b402bb0ca491b
taewok0205@gmail.com <taewok0205@gmail.com>
2026-05-23 15:20:22 +0900

    feat: 현재 tab에 따른 렌더링 조건문 - 홈 tab일 경우에만 MainBannerCarousel 렌더링 - 카테고리 tab일 경우에만 TagSidebar 렌더링

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,8 @@ import { Metadata } from "next";
 import HomeTabContents from "./_components/home-tab-contents";
 import RankingTabContents from "./_components/ranking-tab-contents";
 import { CHARACTERS_DUMMY } from "@/mocks/dummyData";
-import CharacterCreateBanner from "./_components/CharacterCreateBanner";
+import CategoriesTabContents from "./_components/categories-tab-contents";
+import TagSidebar from "./_components/categories-tab-contents/_components/tag-sidebar";
 
 export const metadata: Metadata = {
   title: "home",
@@ -25,37 +26,46 @@ const Home = async ({ searchParams }: HomePageProps) => {
   const TabComponents: { [key: string]: React.ReactNode } = {
     all: <HomeTabContents charArray={CHARACTERS_DUMMY} />,
     ranking: <RankingTabContents />,
+    categories: <CategoriesTabContents />,
     // new: <NewContent />,
     // official: <OfficialContent />,
   };
 
+  const isCategories = currentTab === "categories";
   return (
     <article
       id="home-container"
       className={cn(
-        "w-full min-h-screen", // w-screen 대신 모바일 가로 스크롤 방지를 위해 w-full 권장
-        "flex flex-col",
+        "w-full min-h-[calc(100vh-60px)]",
+        "flex",
+        isCategories && "bg-bg-darker",
       )}
     >
-      {/* 메인 비주얼/슬라이드 영역 (화면 전체 너비 100% 꽉 채움) */}
-      <MainBannerCarousel />
+      {/* 카테고리 tab전용 태그 탐색 sidebar */}
+      {isCategories && <TagSidebar />}
 
-      {/* 하단 콘텐츠 영역 전체를 감싸는 컨테이너 (중앙 정렬 + 양옆 여백 균등 적용) */}
-      <div className="w-full max-w-300 mx-auto @container flex flex-col">
-        {/* 카테고리 필터 영역 */}
-        <MenuTab currentTab={currentTab} />
+      <section
+        className={cn("w-full min-h-[calc(100vh-60px)]", "flex flex-col")}
+      >
+        {/* 메인 비주얼/슬라이드 영역 */}
+        {currentTab === "all" && <MainBannerCarousel />}
 
-        {/* 본문 콘텐츠 + 배너 + 푸터 영역 */}
-        <div className="w-full flex flex-col gap-18">
-          <div id="contents-wrapper" className="flex flex-col gap-15 w-full">
-            {TabComponents[currentTab]}
+        <div className="w-full flex-1 max-w-300 mx-auto @container flex flex-col">
+          {/* 카테고리 필터 영역 */}
+          <MenuTab currentTab={currentTab} />
+
+          {/* 본문+푸터 영역 */}
+          <div className="w-full flex flex-col flex-1">
+            <div id="contents-wrapper" className="flex flex-col w-full flex-1">
+              {TabComponents[currentTab]}
+            </div>
+
+            <div className="mt-auto">
+              <Footer />
+            </div>
           </div>
-
-          <CharacterCreateBanner />
-
-          <Footer />
         </div>
-      </div>
+      </section>
     </article>
   );
 };
