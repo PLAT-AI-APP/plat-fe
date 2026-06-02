1a6b58cf820cedfddda1e2014248b05e788e0797
taewok0205@gmail.com <taewok0205@gmail.com>
2026-06-02 15:50:09 +0900

    style: 버튼 비황성화 디자인 적용

--- src/components/ActiveButton.tsx
+++ src/components/ActiveButton.tsx
@@ -24,10 +24,10 @@ const ActiveButton = ({
       type={type}
       disabled={!isActive}
       className={cn(
-        "w-full h-11.5 title-3 text-font-4 rounded-lg border border-border-main transition-all",
+        "w-full h-11.5 title-3 rounded-lg border border-border-main transition-all",
         isActive
-          ? "bg-brand cursor-pointer"
-          : "bg-font-disabled cursor-not-allowed",
+          ? "bg-brand text-font-4 cursor-pointer"
+          : "bg-card text-font-1 cursor-not-allowed",
         className,
       )}
       {...props}
