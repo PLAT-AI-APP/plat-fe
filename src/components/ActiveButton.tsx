e76a0f3a5867f9cbe8fb0ef84034368e13cb5761
taewok0205@gmail.com <taewok0205@gmail.com>
2026-06-08 01:21:07 +0900

    style: ActiveButton border 삭제

--- src/components/ActiveButton.tsx
+++ src/components/ActiveButton.tsx
@@ -24,7 +24,7 @@ const ActiveButton = ({
       type={type}
       disabled={!isActive}
       className={cn(
-        "w-full h-11.5 title-3 rounded-lg border border-border-main transition-all",
+        "w-full h-11.5 title-3 rounded-lg transition-all",
         isActive
           ? "bg-brand text-font-4 cursor-pointer"
           : "bg-card text-font-1 cursor-not-allowed",
