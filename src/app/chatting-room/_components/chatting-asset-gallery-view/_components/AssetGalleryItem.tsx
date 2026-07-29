6fc4b00be329bdbb90f008fde172a127bfc2b998
taewok0205@gmail.com <taewok0205@gmail.com>
2026-07-29 19:56:24 +0900

    style: 에셋 흐림 효과 모서리 문제 해결

--- src/app/chatting-room/_components/chatting-asset-gallery-view/_components/AssetGalleryItem.tsx
+++ src/app/chatting-room/_components/chatting-asset-gallery-view/_components/AssetGalleryItem.tsx
@@ -13,7 +13,7 @@ const AssetGalleryItem = ({ asset }: AssetGalleryItemProps) => {
   return (
     <button
       type="button"
-      className="relative block aspect-square w-full min-w-0 overflow-hidden rounded-xl bg-[#d9d9d9]"
+      className="relative isolate block aspect-square w-full min-w-0 overflow-hidden rounded-xl bg-[#d9d9d9]"
       aria-disabled={asset.isLocked}
     >
       <Image
@@ -22,15 +22,15 @@ const AssetGalleryItem = ({ asset }: AssetGalleryItemProps) => {
         fill
         sizes="(max-width: 400px) calc((100vw - 49px) / 2), 152px"
         className={cn(
-          "rounded-xl object-cover",
+          "object-cover",
           asset.isLocked && "scale-105 blur-[4px]",
         )}
       />
 
       {asset.isLocked && (
         <>
-          <span className="absolute inset-0 rounded-xl bg-black/50" />
-          <span className="absolute inset-0 flex items-center justify-center">
+          <span className="absolute inset-0 z-10 bg-black/50" />
+          <span className="absolute inset-0 z-20 flex items-center justify-center">
             <LockLine className="size-[30px] text-white" />
           </span>
         </>
