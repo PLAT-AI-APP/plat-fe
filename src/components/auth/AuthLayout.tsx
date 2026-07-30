d4634df3451c8c910c6e98c2d8dc1425aa2188f7
taewok0205@gmail.com <taewok0205@gmail.com>
2026-07-30 16:45:08 +0900

    style: AuthLayout scroll제한 삭제

--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -5,7 +5,7 @@ interface LoginLayoutProps {
 }
 const AuthLayout = ({ children }: LoginLayoutProps) => {
   return (
-    <article className="overflow-hidden relative w-full h-[calc(100vh-60px)] flex items-center justify-center isolate bg-bg-dark">
+    <article className="relative flex min-h-[calc(100vh-60px)] w-full items-center justify-center bg-bg-dark">
       {children}
     </article>
   );
