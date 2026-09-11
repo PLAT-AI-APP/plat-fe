a894a79366658cf99f47895d2cb6598125a19864
taewok0205@gmail.com <taewok0205@gmail.com>
2026-09-12 01:33:58 +0900

    refactor: 라우트 세그먼트별 error.tsx 경계 제거
    렌더 예외를 세그먼트마다 가두던 error.tsx 16개와 그 공통 본문인
    RouteError를 정리한다. 이제 렌더 예외는 전역 경계(global-error.tsx)까지
    올라가 헤더·사이드바를 포함한 화면 전체가 대체된다.

--- src/components/state/index.ts
+++ src/components/state/index.ts
@@ -4,4 +4,3 @@ export { default as QueryStateBoundary } from "./QueryStateBoundary";
 export { default as InfiniteQueryBoundary } from "./InfiniteQueryBoundary";
 export { default as ListErrorRow } from "./ListErrorRow";
 export { default as RouteLoading } from "./RouteLoading";
-export { default as RouteError } from "./RouteError";
